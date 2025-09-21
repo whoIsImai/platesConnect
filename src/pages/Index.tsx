39a76f941e1174016df3bdf6a449883a86c71166
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-09-21 21:52:25 +0200

    fixed an error

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
 import { Header } from "@/components/Header";
-import { Home } from "@/components/Home";
+import { Home } from "../components/Home";
 import { Features } from "@/components/Features";
 import { HowItWorks } from "@/components/HowItWorks";
 import { CTA } from "@/components/CTA";
